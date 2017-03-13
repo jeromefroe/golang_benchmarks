61d707bb7172aef68f8b6c7e3d6f6ce45b3980a7
Jerome Froelich <jeromefroe@uber.com>
2017-03-13 14:22:07 -0400

    Add unsafe functions and tests

--- write_bytes_vs_string_test.go
+++ write_bytes_vs_string_test.go
@@ -6,6 +6,8 @@ import (
 	"reflect"
 	"testing"
 	"unsafe"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const sampleStr = "It’s the job that’s never started as takes longest to finish"
@@ -53,4 +55,26 @@ func unsafeStrToByte(s string) []byte {
 		Cap:  strHeader.Len,
 	}
 	return *(*[]byte)(unsafe.Pointer(&byteHeader))
+}
+
+func unsafeByteToStr(b []byte) string {
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	strHeader := reflect.StringHeader{
+		Data: sliceHeader.Data,
+		Len:  sliceHeader.Len,
+	}
+	return *(*string)(unsafe.Pointer(&strHeader))
+}
+
+func TestUnsafeStrToByte(t *testing.T) {
+	s := "fizzbuzz"
+	expected := []byte(s)
+	assert.Equal(t, expected, unsafeStrToByte(s))
+}
+
+func TestUnsafeByteToStr(t *testing.T) {
+	expected := "fizzbuzz"
+	bytes := []byte(expected)
+	assert.Equal(t, expected, unsafeByteToStr(bytes))
+
 }
